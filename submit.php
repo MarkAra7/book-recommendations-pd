dc4627f3b2cd561c935815c218172e066df609fb
Edgars G <Edgars@gmail>
2025-09-24 11:27:56 +0300

    4.phpuzdevums Datu Bazes savienošanās

--- submit.php
+++ submit.php
@@ -1,5 +1,17 @@
 <?php
+$servername = "localhost";
+$username = "book_review_user_24092025";
+$password = "password";
+$database = "book_review_24092025";
 
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully\n";
 
 
 

1bdd35bf45a7dedc04a7e6ca55c1e8162257529e
Edgars G <Edgars@gmail>
2025-09-24 12:17:56 +0300

    pieliku error

--- Review.php
+++ Review.php
@@ -3,6 +3,10 @@
 class Review{
 
     // Nomainiju uz public ,lai varetu datus dabut ara bez getter un setter
+    //[::1]:50382 [200]: POST /submit.php - Uncaught Error: Cannot access private property Review::$book_title in C:\Users\User\MarkussArājums\book-recommendations-pd\submit.php:27
+//Stack trace:
+//0 {main}
+ // thrown in C:\Users\User\MarkussArājums\book-recommendations-pd\submit.php on line 27
     public string $full_name  ;    
     public string $book_title   ;   
    public string  $review_text  ; 
